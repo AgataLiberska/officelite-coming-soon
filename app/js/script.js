0cbbc0b7254d0daedfe637ad7f8eb789572201e2
Agata Liberska <liberska@gmail.com>
2021-02-16 21:42:18 +0000

    add rotation of toggle icon when select options are open

--- app/js/script.js
+++ app/js/script.js
@@ -1,22 +1,29 @@
 const selectToggle = document.querySelector('.form__control_custom-select__toggle');
-const selectOptions = document.querySelector('.toggleable');
-const html = document.querySelector('html');
+const toggleIcon = document.querySelector('.form__control_custom-select__toggle__icon');
+const selectList = document.querySelector('.toggleable');
+const selectOptions = document.querySelector('form__control_custom-select__item')
+// const html = document.querySelector('html');
 
 selectToggle.addEventListener('click', () => {
-    if (selectOptions.classList.contains('open-toggle')) {
-        selectOptions.classList.remove('open-toggle');
-        selectOptions.classList.add('close-toggle');
-    } 
-    else {
-        selectOptions.classList.remove('close-toggle');
-        selectOptions.classList.add('open-toggle')
-    }
-})
+        if (selectList.classList.contains('open-toggle')) {
+            selectList.classList.remove('open-toggle');
+            selectList.classList.add('close-toggle');
+            toggleIcon.classList.remove('is-open');
+        } 
+        else {
+            selectList.classList.remove('close-toggle');
+            selectList.classList.add('open-toggle');
+            toggleIcon.classList.add('is-open');
+        }
+    })
 
-html.addEventListener('click', (e)=> {
-    if (e.target !== selectToggle) {
-        selectOptions.classList.remove('open-toggle');
-    }
-})
+
+    // html.addEventListener('click', (e)=> {
+    //     if (e.target !== selectToggle) {
+    //         console.log(e.target.parentNode)
+    //         selectList.classList.remove('open-toggle');
+    //     }
+    // })
+
 
 console.log('hello')
