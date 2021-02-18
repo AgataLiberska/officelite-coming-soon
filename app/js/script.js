9799b8f001bea327fa7f9e26cb017b7323aa9db3
Agata Liberska <liberska@gmail.com>
2021-02-18 14:55:03 +0000

    add form validation script

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,5 @@
+// ========= CUSTOM SELECT DROPDOWN ==========================
+
 const customSelectTrigger = document.querySelectorAll('.custom-select-trigger');
 const customSelectArrow = document.querySelector('.form__control--custom-select__toggle__icon');
 const customSelectDropdown = document.querySelector('.form__control--custom-select__dropdown');
@@ -8,8 +10,7 @@ customSelectOptions.forEach(option => {
     option.setAttribute('tabindex', '-1');
 })
 
-
-// EVENT LISTENERS
+// EVENT LISTENERS -----------------------------------------------
 
 // open & close dropdown
 customSelectTrigger.forEach(trigger => {
@@ -41,6 +42,8 @@ customSelectOptions.forEach(option => {
     })
 })
 
+// FUNCTIONS -----------------------------------------------------
+
 function openDropdown() {
     customSelectDropdown.classList.remove('close-dropdown');
     customSelectDropdown.classList.add('open-dropdown');
@@ -52,3 +55,74 @@ function closeDropdown() {
     customSelectDropdown.classList.add('close-dropdown');
     customSelectArrow.classList.remove('is-open');
 }
+
+
+// =========== FORM VALIDATION ====================================
+// Name is required, Email is required, Email needs to be email
+
+const form = document.querySelector('.form');
+const nameInput = document.querySelector('.js-form-name');
+const emailInput = document.querySelector('.js-form-email');
+
+// EVENT LISTENERS ------------------------------------------------
+
+// Check validity of the form on submit
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // if name is empty
+    if (nameInput.validity.valueMissing) {
+        showError(nameInput);
+    }
+    // if email is empty
+    if (emailInput.validity.valueMissing) {
+        showError(emailInput);
+    } 
+    // if email is not an email format
+    else if (!isEmail(emailInput)) {
+        showError(emailInput);
+    }
+})
+
+
+// Check validity of fields on input
+form.addEventListener('input', () => {
+    // name not empty
+    if (nameInput.validity.valid) {
+        removeError(nameInput);
+    }
+    // email is in email format
+    if (isEmail(emailInput)) {
+        removeError(emailInput);
+    }
+})
+
+
+// FUNCTIONS -----------------------------------------------------
+
+function showError(input) {
+    input.classList.add('not-valid');
+    displayErrorIcon(input);
+}
+
+function removeError(input) {
+    input.classList.remove('not-valid');
+    hideErrorIcon(input);
+}
+
+function displayErrorIcon(input) {
+    let formGroup = input.parentElement;
+    let errorIcon = formGroup.querySelector('.form__control__error');
+    errorIcon.classList.remove('hidden');
+}
+
+function hideErrorIcon(input) {
+    let formGroup = input.parentElement;
+    let errorIcon = formGroup.querySelector('.form__control__error');
+    errorIcon.classList.add('hidden');
+}
+
+function isEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email.value);
+}
+
